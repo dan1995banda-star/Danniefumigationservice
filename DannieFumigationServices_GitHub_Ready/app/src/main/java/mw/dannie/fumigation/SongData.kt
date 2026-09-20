526b2ad758e7799fb38ef148ae2ab5f06741d105
dan1995banda-star <dan1995banda@gmail.com>
2026-09-20 06:02:42 +0200

    Update SongData.kt

--- DannieFumigationServices_GitHub_Ready/app/src/main/java/mw/dannie/fumigation/SongData.kt
+++ DannieFumigationServices_GitHub_Ready/app/src/main/java/mw/dannie/fumigation/SongData.kt
@@ -104,6 +104,394 @@ Kwa iye wolakika, adzakhala moyo;
 Pamodzi ndi Mfumuyo dzakhala kosatha.
                 """.trimIndent()
             )
+        ),
+
+        Song(
+            number = 4,
+            title = "AKHRISTU ALI NACHOTU CHIYEMBEKEZO",
+            englishTitle = "How Cheering",
+            verses = listOf(
+                """
+Akhristu ali nachotu chiyembekezocho,
+Chimatikondweretsatu m’mabvuto monsemo,
+Chimatikondweretsatu m’mabvuto monsemo.
+                """.trimIndent(),
+
+                """
+Chinena za mpumulowo pokhala ndi Khristu
+Pamodzi ndi anzangawo tidzakhalitsako,
+Pamodzi ndi anzangawo tidzakhalitsako.
+                """.trimIndent(),
+
+                """
+Mayeso ndi machimowo kulibe mdzikolo,
+Misozi ndipo imfanso kulibe komweko,
+Misozi ndipo imfanso kulibe komweko.
+                """.trimIndent(),
+
+                """
+Masiku athe msangatu, ambuye adzetu,
+Tilakalaka kuona dzikolo ndi Inu,
+Tilakalaka kuona dzikolo ndi Inu.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 5,
+            title = "AKHRISTU NDI ABALE",
+            englishTitle = "Christians are Brothers",
+            verses = listOf(
+                """
+Akhristu ndi abale, tidziyenderana tonse,
+Mulungu m’modzi tiri naye, ndi Ambuye yemwe,
+Chitsime chomwe, njira yomwe yokhulupirira.
+Tiyeni tonse, m’mpingo m‘modzi tidziyenderana.
+                """.trimIndent(),
+
+                """
+Atate yemwe anatuma Mpulumutsi m’modzi;
+Kudzatifera kuti atiyanjanitse tonse,
+Ndi Mzimu yemwe ayeretsa akumvera onse.
+Tiyeni tonse, m’mpingo m‘modzi tidziyenderana.
+                """.trimIndent(),
+
+                """
+Anatipatsa mau omwe alimbitsa mtima;
+Kumwamba komwe timka ndi kulondol njira yomwe
+Tibalalikiranji ife akutsata Yesu
+Tiyeni tonse mpingo ‘modzi tidziyenderana
+                """.trimIndent(),
+
+                """
+Satana apambana tikayamba kusiyana;
+Tikayanjana, yemwe adzagonja ndi kuthawa;
+Mbuyathu adzamthyola konse ndi kumlamulira.
+Tiyeni tonse, m’mpingo m’modzi tidziyenderana.
+                """.trimIndent(),
+
+                """
+Tiphatikane tonse ‘fe okonda Yesu Khristu;
+Tisalekane pakugwira ntchito za Mbuyathu;
+Tilimbitsane pakuyenda m’njira yonka kwathu.
+Tiyeni tonse, m’mpingo ‘modzi tidziyenderana.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 6,
+            title = "ALIKUITANA UKO TSIDYALO",
+            englishTitle = "There’s a Call Comes Ringing",
+            verses = listOf(
+                """
+Alikuitana uko tsidyalo, tumiza mauwo,
+Iwo akufuna Mpulumutsi, tumiza mauwo.
+Tibukitse mau akewo, ndi mitima yakufunatu,
+Pakumlemekeza Mulunguyo; ndi kusunga mau akewo,
+Tuma mau a uthengawo, kulikonse amuetu,
+Tuma mau a uthengawo, osalake konse bi.
+                """.trimIndent(),
+
+                """
+Tamva kuitana tsidya lijalo, tumiza mauwo,
+Tipereke za chifundo zathuzo, tumiza mauwo.
+                """.trimIndent(),
+
+                """
+Tipempheretu chisomo chakecho, tumiza mauwo,
+Mzimu wa Ambuye anke ponsepo, tumiza mauwo.
+                """.trimIndent(),
+
+                """
+Tisaleme pakuchita eabwino, tumiza mauwo,
+Pakukopa anthu osokerawo, tumiza mauwo.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 7,
+            title = "ALIPO MALO OPUMA",
+            englishTitle = "There’s a Place for Great Rest",
+            verses = listOf(
+                """
+Alipo malo opuma, mfupi ndi Mulungu,
+Kumene tchimo kulibe, mfupi ndi Mulungu.
+O! Yesu Mpulumutsi, mtima wa Mulungu,
+Tisungeni pompano, mfupi ndi Mulungu.
+                """.trimIndent(),
+
+                """
+Alipo malo okoma, mfupi ndi Mulungu,
+Pakukomana ndi Mbuye, mfupi ndi Mulungu.
+                """.trimIndent(),
+
+                """
+Alipo malo okwana, mfupi ndi Mulungu,
+Chimwemwe ndi mtenderewo, mfupi ndi Mulungu.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 8,
+            title = "AMBIRI TIKONDWERA MWA YESU",
+            englishTitle = "Many are Happy in Jesus",
+            verses = listOf(
+                """
+Ambiri tikondwera mwa Yesu, Nanga inu? Nanga inu?
+Tapeza chipulumutso ndithu; Nanga inu?
+Masiku onse timakondwera, Pakumva mau ake a Yesu;
+Wolalikira tidzamumvera; Nanga inu?
+                """.trimIndent(),
+
+                """
+Ife tiyembekezera Yesu, Nanga inu? Nanga inu?
+Chimwemwecho chiri mumtimamu; Nanga inu?
+                """.trimIndent(),
+
+                """
+Titasokera tinabwerera, Nanga inu? Nanga inu?
+Tadziwa kuti ndiwo uchimo; Nanga inu?
+                """.trimIndent(),
+
+                """
+Timachoka m'njirayo ya imfa, Nanga inu? Nanga inu?
+Takopeka ndi mau a Yesu; Nanga inu?
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 9,
+            title = "A! MBUSA WANGA NDIMVATU",
+            englishTitle = "Hark Tis The Saviour’s Voice",
+            verses = listOf(
+                """
+A! Mbusa wanga ndimvatu, Uko kuchipululuko;
+Zilira nkhosa zaketu, Ziri kutali m’kholamo.
+Bwezatu, bwezatu, Bweza zonsezo m’opsyamo;
+Bwezatu, bwezatu, Bweza zakezo kwa Yesu.
+                """.trimIndent(),
+
+                """
+Ndani afuna kunkako, Kukazipeza nkhosazo?
+Ndani adzathandizayo, Kuti zifunde m’kholamo?
+                """.trimIndent(),
+
+                """
+Etu! Zilira konseko, Ziri ku phiri ko’psyako,
+Imva, anena Mwiniyo, Kweye upeze nkhosazo.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 10,
+            title = "AMBUYANGA YESU, NDIKONDA INU",
+            englishTitle = "My Jesus I Love Thee",
+            verses = listOf(
+                """
+Ambuyanga Yesu, Ndikonda Inu,
+Ndisiya zoipa chifukwa chanu,
+Ambuye wabwino ndi Mpulumutsiyo;
+Ndikondwa tsopano koposa kale.
+                """.trimIndent(),
+
+                """
+Ndikonda chifukwa munakonda ‘ne;
+Mnafera pa mtengo kuombola ‘ne,
+Mukhululukira zoipa zonse;
+Ndikonda tsopano koposa kale.
+                """.trimIndent(),
+
+                """
+Ndidzakukondani kosalekai
+Ndidzakutamani masiku onse
+Mpakatu ndidzafa ndidzanenabe
+Ndikonda tsopano koposa kale
+                """.trimIndent(),
+
+                """
+Ku malo owala, kumwamba komwe,
+Ndidzakuongani, Ambuye Yesu;
+Ntabvala korona ndidzayimbabe,
+Ndikonda tsopano koposa kale.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 11,
+            title = "AMBUYE ALI PAFUPI KUDZA",
+            englishTitle = "Tis almost Time for the Lord to Come",
+            verses = listOf(
+                """
+Ambuye ali pafupi kudza, ndikumva chonchodi,
+Ndiponso nyenyezi zikuzima, tsopano ndi mbanda kuchatu,
+Ee! tsopano ndi mbanda kuchatu!
+Ee! tsopano ndi mbanda kucha ndithu!
+Usiku wathadi! Usana wadzanso.
+Ee! tsopano ndi mbanda kuchatu!
+                """.trimIndent(),
+
+                """
+Zizindikiro za mlengalenga zilalikiradi;
+Ndi za pa dziko zinena kuti, Ambuye wayandikiratu.
+                """.trimIndent(),
+
+                """
+Ndi nthawi yoti kunyada kuthe mu mpingo wonsewo,
+Ukakhale wofulumira ntchito, ukhale woyang’aniratu.
+                """.trimIndent(),
+
+                """
+Pitani msanga kukalalika kwa osaukawo;
+Ndi kwa omveka a dziko lino, onsewo afulumirenso.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 12,
+            title = "AMBUYE MU IMFAMO MUNAKONDA INE",
+            englishTitle = "Saviour thy Dying Love",
+            verses = listOf(
+                """
+Ambuye m’imfamo munakonda ine;
+Sindinakaniza ‘Nu, kanthu konsei;
+Ndimagwadira ‘Nu, Ndidzaperekatu nsembe yangayi.
+                """.trimIndent(),
+
+                """
+Kumwamba kwanuko mundipempherere;
+Ndi mphamvu yanuyo mndilimbitsetu;
+Mndisenze mtandawo, ndikhale mbaniyo,
+Ndikaperekeko mituloyi.
+                """.trimIndent(),
+
+                """
+Ndikhumba mtimawo wofatsa ndithu,
+Kuti ndiyambe ‘ne ntchito zanuzi;
+Akusokerawo ofuna zaowo,
+Abwere kwaoko, inde, kwanutu.
+                """.trimIndent(),
+
+                """
+Zambiri zangazo mnandipatsa kale,
+Ndibweza zonsezo kwa Mbuyanga ‘Nu,
+Ndikadzaona ‘Nu, Mbuye wabwinotu,
+Nsembe yokomatu ndidzakhalabe.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 13,
+            title = "AMBUYE NDIFUNA NDITSUKENITU",
+            englishTitle = "Lord Jesus I Long to be Perfectly Whole",
+            verses = listOf(
+                """
+Ambuye ndifuna nditsukenitu,
+Mukhalitse ndine mu mtima’ngamu;
+Muthyole zonsezo zodetsa ‘netu,
+Musambitse ine, ndiyeretu mbu!
+Ndiyere mbu, E, ndiyere mbu,
+Musambitse Ine, ndiyeretu mbu.
+                """.trimIndent(),
+
+                """
+Ambuye taonani pansi pano,
+Kuti m’ndithandize kudzipereka;
+Ndileke zangazo zoipsa’netu,
+Musambitse ine, ndiyeretu mbu.
+                """.trimIndent(),
+
+                """
+Chifukwa cha ichi ndipempheratu,
+Ndidikira Inu Ambuya’nga ‘Nu;
+Ndikhulupirira pa mwaziwo psyu,
+Musambitse ine, ndiyeretu mbu.
+                """.trimIndent(),
+
+                """
+Ambuye muona ndidikira ‘Nu,
+Idzani kukonzetsa mtima wanga;
+Omwe akufuna simuwakana,
+Musambitse Ine, ndiyeretu mbu.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 14,
+            title = "AMBUYE WATHU YESU AKHALA MFUMUNSO",
+            englishTitle = "The King of Glory Standeth",
+            verses = listOf(
+                """
+Ambuye wathu Yesu akhala Mfumunso;
+Zoipa zazikulu azigonjetsa ‘zo,
+Anena mau Ake ochotsa zonsezo,
+Kalelo nalamula: “Pakhale bata du.”
+                """.trimIndent(),
+
+                """
+M’ulemerero mwake anenanenabe;
+Munkhondo apambana ndi mau akewo,
+Akhristu akondwera poona mphamvuyo
+Ya Ambuye wathu Yesu wotiombolayo.
+                """.trimIndent(),
+
+                """
+Zobvala zake zonse nza mwazi woti psyu;
+Pamphumi pake pomwe korona waketu,
+Unyolo wa Satana umasulidwako;
+Kumwamba amithenga atama Mbuyeyo.
+                """.trimIndent(),
+
+                """
+M’kufatsa kwake komwe anena nafenso,
+Tonse‘fe omangidwa ndi zakuipazi;
+Ambuye akunena zotichenjezazi:
+“Ananu, msiye zonse mupulumuketu.”
+                """.trimIndent(),
+
+                """
+Chikondi cha Mbuyathu chiposa zonsetu;
+Chisomo chake chomwe chikondweretsabe;
+Ulemu wake wonse utopambanako;
+Tiyeni, tikondwere popita m’mwambamo.
+                """.trimIndent()
+            )
+        ),
+
+        Song(
+            number = 15,
+            title = "AMBUYE WATHU YESU, MUBWERA LITI KO?",
+            englishTitle = "How Long?",
+            verses = listOf(
+                """
+Ambuye wathu Yesu, mubwera liti ko?
+Mitima yotopadi, muchedweranji ko?
+Lidzadza liti tsiku lonyezimiralo?
+Limene anthu anu adzakondweranso?
+                """.trimIndent(),
+
+                """
+Musiye mpaka liti, Ee, nyumba yanuyo?
+Mwachedwachedwa, Mbuye, ambiri agwatu;
+Agwidwa ndi za dziko, antchito anuwo;
+Atsalatu pang'ono oyang’anira ‘Nu.
+                """.trimIndent(),
+
+                """
+Dzutsani anthu anu, tumani mauwo;
+Anene anthu onse, akudza Mbuyeyo?
+Ziyake nyali zathu, tibvale bwinotu,
+Tikonze mtima wathu, kuona Mbuyeyo.
+                """.trimIndent()
+            )
         )
     )
 }
